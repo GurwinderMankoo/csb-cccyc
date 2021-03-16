b2c465975afdd5180f37de74d86317916cad7e6e
Code Sandbox <hello@codesandbox.io>
2021-03-16 11:39:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.03b7af18b960b4fb2eefc71d292621df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf25489a6e603ed722d961076e4e73c",
+    "url": "https://GurwinderMankoo.github.io/csb-cccyc/index.html"
+  },
+  {
+    "revision": "cb164c76ead19cf933d4",
+    "url": "https://GurwinderMankoo.github.io/csb-cccyc/static/css/main.41b5d104.chunk.css"
+  },
+  {
+    "revision": "330a394bf5d00d4ea6fd",
+    "url": "https://GurwinderMankoo.github.io/csb-cccyc/static/js/2.771d1ace.chunk.js"
+  },
+  {
+    "revision": "cb164c76ead19cf933d4",
+    "url": "https://GurwinderMankoo.github.io/csb-cccyc/static/js/main.65fb4e65.chunk.js"
+  },
+  {
+    "revision": "6c9645770bffba4f9787",
+    "url": "https://GurwinderMankoo.github.io/csb-cccyc/static/js/runtime~main.91436971.js"
+  }
+]);
